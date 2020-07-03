e73e04973bef300784d398affcd4687d90452f99
sachafrosell <sachafrosell@gmail.com>
2020-07-03 18:20:22 +0100

    Updates

---
+++ precache-manifest.e3d350189b3fa76db11bbaef9983ada0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff569f9aef0fcde9ebc6698e936f9b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a0ca46ccd27efb3732",
+    "url": "/static/css/main.64cdcd56.chunk.css"
+  },
+  {
+    "revision": "0031ee09199a0abb3b3e",
+    "url": "/static/js/2.9b4c70f1.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.9b4c70f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a0ca46ccd27efb3732",
+    "url": "/static/js/main.7f8f7796.chunk.js"
+  },
+  {
+    "revision": "e09fadcf3ca21c500de2",
+    "url": "/static/js/runtime-main.0dd902e1.js"
+  },
+  {
+    "revision": "15c05b7279449a4c1fbeb4ff47641133",
+    "url": "/static/media/800px_COLOURBOX25316311.15c05b72.jpg"
+  },
+  {
+    "revision": "2864ab5d009cf0ee77a99246fe2495ee",
+    "url": "/static/media/Eric.2864ab5d.png"
+  },
+  {
+    "revision": "367bb1249233e66d884eb05ec361b8e6",
+    "url": "/static/media/Felix.367bb124.png"
+  },
+  {
+    "revision": "7b180c0f46aa3b864b3ac6177bb3b520",
+    "url": "/static/media/John.7b180c0f.png"
+  },
+  {
+    "revision": "346d2c0e4e5733bcb4f2761bd7265e82",
+    "url": "/static/media/bede.346d2c0e.png"
+  },
+  {
+    "revision": "5fadfd473b10d1eb6484f1053250dc0e",
+    "url": "/static/media/city1.5fadfd47.jpg"
+  },
+  {
+    "revision": "8c61382500f2a9a671e85e03389a3993",
+    "url": "/static/media/city3.8c613825.jpg"
+  },
+  {
+    "revision": "9c40e369652b695d826b184d079063a9",
+    "url": "/static/media/city4.9c40e369.jpg"
+  },
+  {
+    "revision": "de4fdd9ca7a031cd81261d5408ade240",
+    "url": "/static/media/jo-hurst-croft.de4fdd9c.jpg"
+  },
+  {
+    "revision": "0fd4fd480462293c04b1ef45f25c833c",
+    "url": "/static/media/joanna-cheong.0fd4fd48.jpg"
+  },
+  {
+    "revision": "d8ad5e380efa90c2f4c98fc929f68b42",
+    "url": "/static/media/karl-boyce.d8ad5e38.jpg"
+  },
+  {
+    "revision": "fa0e7ae1bc4ec4133b902b1adcdbf32c",
+    "url": "/static/media/kigali-city-4.fa0e7ae1.jpg"
+  },
+  {
+    "revision": "6367ecf3be629f421abf644e6a8e6ae0",
+    "url": "/static/media/lake-burera.6367ecf3.jpg"
+  },
+  {
+    "revision": "a131465d4dded28cdaf8b6ef5a25b230",
+    "url": "/static/media/logo_dark_ride.a131465d.png"
+  },
+  {
+    "revision": "a3c2014b62d89df309536bea3d823ffd",
+    "url": "/static/media/logo_light_ride.a3c2014b.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "24fe03c1c679cd37e95b907ff21a3168",
+    "url": "/static/media/richard-bouma.24fe03c1.jpg"
+  },
+  {
+    "revision": "72649299c2322e683d0a3f2871a48f2a",
+    "url": "/static/media/road1.72649299.jpg"
+  },
+  {
+    "revision": "3d21285c4b1fc24b557ea94f4c83285d",
+    "url": "/static/media/tuk-tuk2.3d21285c.jpg"
+  },
+  {
+    "revision": "2de63d4a4fc1b8c90d8efb7556e1d117",
+    "url": "/static/media/tuk-tuk22.2de63d4a.jpg"
+  }
+]);
