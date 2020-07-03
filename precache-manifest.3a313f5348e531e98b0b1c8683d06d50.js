f02cee16eda3d396e4b133f084414d647ed85aec
sachafrosell <sachafrosell@gmail.com>
2020-07-03 18:57:44 +0100

    Updates

---
+++ precache-manifest.3a313f5348e531e98b0b1c8683d06d50.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f110d145422bec7163274e4c56672a5",
+    "url": "/arc-website/index.html"
+  },
+  {
+    "revision": "c7a7d2c6581bb23917ea",
+    "url": "/arc-website/static/css/main.3705e35b.chunk.css"
+  },
+  {
+    "revision": "c2f7109d40b83b0fcc84",
+    "url": "/arc-website/static/js/2.1d777172.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/arc-website/static/js/2.1d777172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a7d2c6581bb23917ea",
+    "url": "/arc-website/static/js/main.bb32e01c.chunk.js"
+  },
+  {
+    "revision": "a62887c29b1ff2248f32",
+    "url": "/arc-website/static/js/runtime-main.a63cf560.js"
+  },
+  {
+    "revision": "15c05b7279449a4c1fbeb4ff47641133",
+    "url": "/arc-website/static/media/800px_COLOURBOX25316311.15c05b72.jpg"
+  },
+  {
+    "revision": "2864ab5d009cf0ee77a99246fe2495ee",
+    "url": "/arc-website/static/media/Eric.2864ab5d.png"
+  },
+  {
+    "revision": "367bb1249233e66d884eb05ec361b8e6",
+    "url": "/arc-website/static/media/Felix.367bb124.png"
+  },
+  {
+    "revision": "7b180c0f46aa3b864b3ac6177bb3b520",
+    "url": "/arc-website/static/media/John.7b180c0f.png"
+  },
+  {
+    "revision": "346d2c0e4e5733bcb4f2761bd7265e82",
+    "url": "/arc-website/static/media/bede.346d2c0e.png"
+  },
+  {
+    "revision": "5fadfd473b10d1eb6484f1053250dc0e",
+    "url": "/arc-website/static/media/city1.5fadfd47.jpg"
+  },
+  {
+    "revision": "8c61382500f2a9a671e85e03389a3993",
+    "url": "/arc-website/static/media/city3.8c613825.jpg"
+  },
+  {
+    "revision": "9c40e369652b695d826b184d079063a9",
+    "url": "/arc-website/static/media/city4.9c40e369.jpg"
+  },
+  {
+    "revision": "de4fdd9ca7a031cd81261d5408ade240",
+    "url": "/arc-website/static/media/jo-hurst-croft.de4fdd9c.jpg"
+  },
+  {
+    "revision": "0fd4fd480462293c04b1ef45f25c833c",
+    "url": "/arc-website/static/media/joanna-cheong.0fd4fd48.jpg"
+  },
+  {
+    "revision": "d8ad5e380efa90c2f4c98fc929f68b42",
+    "url": "/arc-website/static/media/karl-boyce.d8ad5e38.jpg"
+  },
+  {
+    "revision": "fa0e7ae1bc4ec4133b902b1adcdbf32c",
+    "url": "/arc-website/static/media/kigali-city-4.fa0e7ae1.jpg"
+  },
+  {
+    "revision": "6367ecf3be629f421abf644e6a8e6ae0",
+    "url": "/arc-website/static/media/lake-burera.6367ecf3.jpg"
+  },
+  {
+    "revision": "a131465d4dded28cdaf8b6ef5a25b230",
+    "url": "/arc-website/static/media/logo_dark_ride.a131465d.png"
+  },
+  {
+    "revision": "a3c2014b62d89df309536bea3d823ffd",
+    "url": "/arc-website/static/media/logo_light_ride.a3c2014b.png"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/arc-website/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/arc-website/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/arc-website/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/arc-website/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "24fe03c1c679cd37e95b907ff21a3168",
+    "url": "/arc-website/static/media/richard-bouma.24fe03c1.jpg"
+  },
+  {
+    "revision": "72649299c2322e683d0a3f2871a48f2a",
+    "url": "/arc-website/static/media/road1.72649299.jpg"
+  },
+  {
+    "revision": "3d21285c4b1fc24b557ea94f4c83285d",
+    "url": "/arc-website/static/media/tuk-tuk2.3d21285c.jpg"
+  },
+  {
+    "revision": "2de63d4a4fc1b8c90d8efb7556e1d117",
+    "url": "/arc-website/static/media/tuk-tuk22.2de63d4a.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e3d350189b3fa76db11bbaef9983ada0.js"
+  "/arc-website/precache-manifest.3a313f5348e531e98b0b1c8683d06d50.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arc-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
